ee0a73e3f3208ee738e35ebc0d2ec6b84aadbc61
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-05-29 04:18:31 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -22,7 +22,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `   
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ❄
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 ❄
  ┃➤
  ┃➤    𝗛ola, ${taguser}
  ┃➤
@@ -32,14 +32,14 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ Fᴇᴄʜᴀ : ${date}
  ╰━━━━━━━━━━━━━
   
- ╭━      ❄ 𝑰𝒏𝒇𝒐 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ❄ 
+ ╭━      ❄ 𝑰𝒏𝒇𝒐 𝑭𝒆𝒓𝑩𝒐𝒕 ❄ 
  ┃➤ ${usedPrefix}listagrupos
  ┃➤ ${usedPrefix}estado
  ┃➤ ${usedPrefix}creador
  ┃➤ ${usedPrefix}velocidad
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑱𝒖𝒆𝒈𝒐𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑱𝒖𝒆𝒈𝒐𝒔 ❄ 
  ┃➤ ${usedPrefix}mates | matemáticas | math
  ┃➤ ${usedPrefix}lanzar *cara* | *cruz*
  ┃➤ ${usedPrefix}ppt *piedra : papel : tijera*
@@ -84,13 +84,8 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}ruleta
  ┃➤ ${usedPrefix}suerte
  ╰━━━━━━━━━━━━━
-  
- ╭━           ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑰𝑨 ❄ 
- ┃➤ ${usedPrefix}geminiimg *reaccione a una foto*
- ┃➤ ${usedPrefix}gemini *texto*
- ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑨𝒖𝒔𝒕𝒆𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐  ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑨𝒖𝒔𝒕𝒆𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐  ❄ 
  ┃➤ ${usedPrefix}enable *o* disable *bienvenida | welcome*
  ┃➤ ${usedPrefix}enable *o* disable *avisos | detect*
  ┃➤ ${usedPrefix}enable *o* disable *autonivel | autolevelup*
@@ -120,7 +115,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}enable *o* disable *sologrupos | gconly*  
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑫𝒆𝒔𝒄𝒂𝒓𝒈𝒂𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑫𝒆𝒔𝒄𝒂𝒓𝒈𝒂𝒔 ❄ 
  ┃➤ ${usedPrefix}imagen | image *texto*
  ┃➤ ${usedPrefix}pinterest | dlpinterest *texto*
  ┃➤ ${usedPrefix}wallpaper|wp *texto*
@@ -150,15 +145,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}historia
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑨𝒏𝒐𝒏𝒊𝒎𝒐 ❄ 
- ┃➤ ${usedPrefix}chatanonimo | anonimochat
- ┃➤ ${usedPrefix}anonimoch
- ┃➤ ${usedPrefix}start
- ┃➤ ${usedPrefix}next
- ┃➤ ${usedPrefix}leave
-  ╰━━━━━━━━━━━━━
-
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐 ❄ 
  ┃➤ ${usedPrefix}add *numero*
  ┃➤ ${usedPrefix}sacar | ban | kick  *@tag*
  ┃➤ ${usedPrefix}grupo *abrir o cerrar*
@@ -193,7 +180,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}disable
   ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑷𝒂𝒓𝒆𝒋𝒂𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑷𝒂𝒓𝒆𝒋𝒂𝒔 ❄ 
  ┃➤ ${usedPrefix}listaparejas | listship
  ┃➤ ${usedPrefix}mipareja | mylove
  ┃➤ ${usedPrefix}pareja | couple *@tag*
@@ -202,7 +189,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}terminar | finish *@tag*
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑪𝒐𝒏𝒗𝒆𝒓𝒕𝒊𝒅𝒐𝒓𝒆𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑪𝒐𝒏𝒗𝒆𝒓𝒕𝒊𝒅𝒐𝒓𝒆𝒔 ❄ 
  ┃➤ ${usedPrefix}toimg | img | jpg *sticker*
  ┃➤ ${usedPrefix}toanime | jadianime *foto*
  ┃➤ ${usedPrefix}tomp3 | mp3 *video o nota de voz*
@@ -213,12 +200,12 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}tts es *texto*
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 ❄ 
  ┃➤ ${usedPrefix}logos *efecto texto*
  ┃➤ ${usedPrefix}menulogos2
 ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 ❄ 
  *┃➤* _${usedPrefix}simpcard *@tag*_
  *┃➤* _${usedPrefix}hornycard *@tag*_
  *┃➤* _${usedPrefix}lolice *@tag*_
@@ -229,7 +216,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   ╰━━━━━━━━━━━━━
 
  
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑨𝒏𝒊𝒎𝒆 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑨𝒏𝒊𝒎𝒆 ❄ 
  ┃➤ ${usedPrefix}chica
  ┃➤ ${usedPrefix}chico
  ┃➤ ${usedPrefix}cristianoronaldo
@@ -282,7 +269,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}cosplay
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑬𝒇𝒆𝒄𝒕𝒐𝒔 ❄
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑬𝒇𝒆𝒄𝒕𝒐𝒔 ❄
  ┃➤ ${usedPrefix}bass
  ┃➤ ${usedPrefix}blown
  ┃➤ ${usedPrefix}deep
@@ -297,7 +284,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}tupai
  ╰━━━━━━━━━━━━━
   	
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑩𝒖𝒔𝒒𝒖𝒆𝒅𝒂𝒔 ❄
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑩𝒖𝒔𝒒𝒖𝒆𝒅𝒂𝒔 ❄
  ┃➤ ${usedPrefix}animeinfo *texto*
  ┃➤ ${usedPrefix}anuel
  ┃➤ ${usedPrefix}conejo
@@ -312,7 +299,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}wiki | wikipedia *texto*
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑯𝒆𝒓𝒓𝒂𝒎𝒊𝒆𝒏𝒕𝒂𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑯𝒆𝒓𝒓𝒂𝒎𝒊𝒆𝒏𝒕𝒂𝒔 ❄ 
  ┃➤ ${usedPrefix}afk *motivo*
  ┃➤ _${usedPrefix}acortar *url*
  ┃➤ ${usedPrefix}calc *operacion math*
@@ -329,11 +316,11 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ╰━━━━━━━━━━━━━
 
 
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑻𝒐𝒑𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑻𝒐𝒑𝒔 ❄ 
  ┃➤ ${usedPrefix}top
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 ❄ 
  ┃➤ ${usedPrefix}sticker | s *imagen o video*
  ┃➤ ${usedPrefix}sticker | s *url de tipo jpg*
  ┃➤ ${usedPrefix}emojimix *😺+😆*
@@ -353,12 +340,12 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}cs *:* cs2
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 2 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 2 ❄ 
  ┃➤ ${usedPrefix}wm *packname|author*
  ┃➤ ${usedPrefix}wm *texto1|texto2*
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 3 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 3 ❄ 
  ┃➤ ${usedPrefix}palmaditas | pat *@tag*
  ┃➤ ${usedPrefix}bofetada | slap *@tag*
  ┃➤ ${usedPrefix}golpear *@tag*
@@ -366,7 +353,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}alimentar | food *@tag*
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 𝑶𝒘𝒏𝒆𝒓𝒔 ❄ 
+ ╭━     ❄ 𝑭𝒆𝒓𝑩𝒐𝒕 𝑶𝒘𝒏𝒆𝒓𝒔 ❄ 
  ┃➤ ${usedPrefix}join *enlace*
  ┃➤ ${usedPrefix}unete *enlace*
  ┃➤ ${usedPrefix}dardiamantes *cantidad*
