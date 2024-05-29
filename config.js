19c2aa09365f8ab3b55d71bd84847b3581f49a32
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-05-29 04:04:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -11,45 +11,45 @@ global.botnumber = ""
 
 global.owner = [
   ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕', true],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', false],
-  ['5215541081250', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
-  ['5215541081250','  𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕  ', true],
-  ['5215541081250'],
-  ['5215541081250'],
-  ['5215541081250'],
-  ['5215541081250'],
-  ['5215541081250'],
-  ['5215541081250'],
-  ['5215541081250'],
-  ['5215541081250'], 
-  ['5215541081250'],  
-  ['5215541081250'], 
-  ['5215541081250']
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕', true],
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', false],
+  ['5217223931652', ' 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ', true],
+  ['5217223931652','  𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕  ', true],
+  ['5217223931652'],
+  ['5217223931652'],
+  ['5217223931652'],
+  ['5217223931652'],
+  ['5217223931652'],
+  ['5217223931652'],
+  ['5217223931652'],
+  ['5217223931652'], 
+  ['5217223931652'],  
+  ['5217223931652'], 
+  ['5217223931652']
 ];
 
-global.suittag = ['5215541081250'];
-global.prems = ['5215541081250'];
+global.suittag = ['5217223931652'];
+global.prems = ['5217223931652'];
 
-global.packname = '𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕';
-global.author = '𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕';
-global.wm = '𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕';
-global.titulowm = '𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕';
-global.titulowm2 = `𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕`
-global.igfg = '𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕';
-global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
+global.packname = '𝐅𝐞𝐫𝐁𝐨𝐭';
+global.author = '𝐅𝐞𝐫𝐁𝐨𝐭';
+global.wm = '𝐅𝐞𝐫𝐁𝐨𝐭';
+global.titulowm = '𝐅𝐞𝐫𝐁𝐨𝐭';
+global.titulowm2 = `𝐅𝐞𝐫𝐁𝐨𝐭`
+global.igfg = '𝐅𝐞𝐫𝐁𝐨𝐭';
+global.wait = '*[❗]  ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
 
-global.imagen1 = fs.readFileSync('./allmenu.png');
-global.imagen2 = fs.readFileSync('./src/newavatar.png');
-global.imagen3 = fs.readFileSync('./src/newavatar.png');
-global.imagen4 = fs.readFileSync('./allmenu.png');
-global.imagen5 = fs.readFileSync('./src/newavatar.png');
-global.imagen6 = fs.readFileSync('./allmenu.png');
+global.imagen1 = fs.readFileSync('./menufer.png');
+global.imagen2 = fs.readFileSync('./src/avafer.png');
+global.imagen3 = fs.readFileSync('./src/avafer.png');
+global.imagen4 = fs.readFileSync('./menufer.png');
+global.imagen5 = fs.readFileSync('./src/avafer.png');
+global.imagen6 = fs.readFileSync('./menufer.png');
 
 global.mods = [];
 
@@ -62,15 +62,15 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `▸ ${dia} ${fecha}\n 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕`;
-global.gt = '𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕';
-global.mysticbot = '𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕';
+global.wm2 = `▸ ${dia} ${fecha}\n 𝐅𝐞𝐫𝐁𝐨𝐭`;
+global.gt = '𝐅𝐞𝐫𝐁𝐨𝐭';
+global.mysticbot = '𝐅𝐞𝐫𝐁𝐨𝐭';
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
-global.waittt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
-global.waitttt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
-global.nomorown = '5215541081250';
+global.waitt = '*[❗]  ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
+global.waittt = '*[❗]  ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
+global.waitttt = '*[❗]  ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
+global.nomorown = '5217223931652';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
